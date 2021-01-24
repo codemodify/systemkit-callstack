3ec5415ef3c25c5d25492cece43c799968313520
codemodify <codemodify@member.fsf.org>
2021-01-24 01:47:42 -0500

    update to go 15

---
+++ tests/01-reflection_test.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"testing"
+)
+
+func Test_01(t *testing.T) {
+	// var thisFuncName = fmt.Sprintf("THIS FUNC: %s", helpersReflect.GetThisFuncName())
+
+	// t.Log(thisFuncName)
+	// fmt.Println(thisFuncName)
+}
